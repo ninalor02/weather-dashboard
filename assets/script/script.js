72372853b0904ec000446f09b1c42f8c64761389
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-10 15:03:02 -0500

    restyle js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,46 +1,18 @@
 var button = document.getElementById('#btn');
 var inText = document.querySelector("#inputText");
 
-var historyList = function(cityName) {
-    $('.previous-search:contains("' + cityName + '")').remove();
+var requestUrl = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
 
-    var searchHistoryInput = $('<p>');
-    searchHistoryInput.addClass('past-search');
-    searchHistoryInput.text(cityName);
-    //console.log('data', historyInput) // not working //
+fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data); // this works //
 
-    var searchInputContainer = $('<div>');
-    searchInputContainer.addClass('past-search-container');
+	for (var i = 0; i < data.length; i++) {
+	}
 
-    searchInputContainer.append(historyInput);
+	
 
-    var searchHistoryContainer = $('#searchHistoryContainer');
-    searchHistoryContainer.append(searchInputContainer);
-
-    if (saveSearch.length > 0) {
-        var previousSearch = localStorage.getItem('saveSearch');
-        $("#search-input").val("");
-    };
-};
-
-var loadHistory = function() {
-
-    var saveSearchHistory = localStorage.getItem("saveSearch");
-    if (!saveSearchHistory) {
-        return false;
-    }
-
-    saveSearchHistory = JSON.parse(saveSearchHistory);
-    for (var i = 0; i <saveSearchHistory.length; i++) {
-        historyList(saveSearchHistory[i]);
-    }
-};
-
-var currentWeatherContainer = function(cityName) {
-    //get api 
-    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13')
-    .then(function(response) {
-        return response.json();
-    })
-}
-
+  });
