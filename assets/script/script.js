90999d9ba0028b476630f224a2fc0011c2d0c567
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-09 22:55:36 -0500

    redoing html file to more basic

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,50 +1,31 @@
-var apiKey = '20f0a2a25bmsha1132609ec51c03p19afc2jsn69af4cefaa78';
-console.log('data') // added to gitignore file need to delete//
-var saveSearch = [];
-console.log('saveSearch') // added to gitignore file need to delete//
+var button = document.getElementById('#btn');
+var searchInput = 
 
 
-var historyList = function(cityName) {
-    $('.past-search:contains("' + cityName + '")').remove();
-}
+button.addEventListener("click", function() {
+    getData();
+  });
 
-// history entries //
-var historyEntry = $('<p>');
-historyEntry.addClass('previous-search');
-historyEntry.text('cityName');
+async function getdata() {
 
-// container for entry //
+const url = 'https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '20f0a2a25bmsha1132609ec51c03p19afc2jsn69af4cefaa78',
+		'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+	},
+    body: JSON.stringify({
+		text: textToTranslate.value,
+		target: 'ru'
+	})
+};
 
-var entryContainer = $('<div>');
-entryContainer.addClass('entryContainer');
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
 
-
-var historyContainer = $('history-container');
-historyContainer.append(historyEntry);
-
-if(saveSearch.length > 0) {
-    var previousSavedSearches = localStorage.getItem("savedSearches");
-        savedSearches = JSON.parse(previousSavedSearches);
-        console.log('data')
-    }
-
-
-
-
-
-
-var currentWeatherSection = function(cityName) {
-    
-    fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=53.1%2C-0.13')
-    
-        .then(function(response) {
-            return response.json()
-        })
-
-        .then(function(response){
-            return currentWeatherSection(cityName).json
-        
-        }); 
-        
-    }
-    // console.log('test1') // only works outsde of function //
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}}
