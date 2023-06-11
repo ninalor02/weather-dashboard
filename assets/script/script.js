f5ccc540eb24ffe6541d986a7588a595ee923238
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-11 12:26:13 -0500

    add more into html and add jquery into js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,26 +1,23 @@
-let theLastCity = false;
+let previousSearch = false;
 
-$('#sea')
-
-button.addEventListener("click", function() {
-  getData();
+$('#searchButton').on('click', function() {
+let cityValue = $('input').val();
+lastCity = true;
+cityWeatherInfo(cityValue);
+$('input').val('');
+console.log('data', cityWeatherInfo)
 });
 
-async function getdata() {
-  const url = 'https://openweather43.p.rapidapi.com/weather?q=%3CREQUIRED%3E&appid=da0f9c8d90bde7e619c3ec47766a42f4&appid=%3CREQUIRED%3E&units=standard';
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '20f0a2a25bmsha1132609ec51c03p19afc2jsn69af4cefaa78',
-      'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
-    }
-  };
-  
-  try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
-  } catch (error) {
-    console.error(error);
-  }
-}
+$("#cityList").on("click", "button", function(event) {
+  let cityValue = event.target.textContent;
+  theLastCity = false;
+  cityWeatherInfo(cityValue);
+});
+
+
+
+
+
+
+
+
