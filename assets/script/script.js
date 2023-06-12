c387504393bfa4a45d5dcde6ba15673517c8ad0b
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-11 21:50:13 -0500

    added css, fixed html, redo js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,31 +1,5 @@
-var searchButton = document.querySelector('#searchButton');
-var previousSearch = document.querySelector('#previousSearch');
-var cityName = document.querySelector('#cityName');
-var fiveDayForecast = document.querySelector('#fiveDayForecast');
-
-button.addEventListener("click", function() {
-    getData();
-  });
- 
-async function getData() {
-const url = 'https://yahoo-weather5.p.rapidapi.com/weather?location=sunnyvale&format=json&u=f';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '20f0a2a25bmsha1132609ec51c03p19afc2jsn69af4cefaa78',
-		'X-RapidAPI-Host': 'yahoo-weather5.p.rapidapi.com'
-	},
-  body: JSON.stringify({
-		text: textToTranslate.value,
-		target: 'ru'
-	})
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-  cityName.textContent = searchText;
-  console.log(searchText);
-} catch (error) {
-	console.error(error);
-}}
+function getInfo(){
+  const newName = document.getElementById("cityInput");
+  const cityName = document.getElementById("cityName");
+  cityName.innerHTML = "--" + newName.value + "--"
+}
