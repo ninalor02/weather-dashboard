4fc5b4d764aae0276cdfad1321cc18f0e444653f
nina <128232339+ninalor02@users.noreply.github.com>
2023-06-12 23:07:50 -0500

    reformat js, html, css

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # weather-dashboard
 challenge 6
 
+This is the screenshot of my to css file.
 
-https://www.w3schools.com/bootstrap/tryit.asp?filename=trybs_ref_glyph_search&stacked=h
+![Alt text](<assets/images/link to css.jpg>)
+
+This is the screenshot of my link to js.
+
+![Alt text](<assets/images/link to js.jpg>)
+
+This is a 5 Day Weather Forecast!
+
+![Alt text](assets/images/front.jpg)
+
+As the front of the page, is the basic display of it.
+
+This is how it should look like, once you enter in a city.
+
+![Alt text](<assets/images/previous search.jpg>)
+
+AS you can see the bottom black box, that is the previous search box.
+
+Once you press the search button, you should have the result of a current and 5 day forecast of Temp, Wind and Humidity.
+
+- unfortunetly I don't have an image of the completed part.
+
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,71 +1,65 @@
-
-var apiKey = "e028752f56f61eb12ac24546cea7f3f6"
+var apiKey = "2aa5de91e2d58f7baa00946e1b387784"
 var savedSearches = [];
 
-function getInfo(){
+function getInfo() {
   const newName = document.getElementById("cityInput");
   const cityName = document.getElementById("cityName");
   cityName.innerHTML = "--" + newName.value + "--"
+
+
+  fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=2aa5de91e2d58f7baa00946e1b387784")
+    .then(response => response.json())
+    .then(data => {
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Temp").innerHTML = "Temp:" + Number(data.list[i].main.temp_min).toFixed(1) + "°";
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Wind").innerHTML = "Wind:" + Number(data.list[i].wind.speed).toFixed(1) + "%";
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById("day" + (i + 1) + "Humidity").innerHTML = "Humidity" + Number(data.list[i].main.humidity).toFixed(1) + "°";
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById("img" + (i + 1)).src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
+      }
+
+      for (i = 0; i < 5; i++) {
+        document.getElementById('day' + (i + 1)).innerHTML = weekday[checkDay(i)];
+      }
+    })
+    .catch(err => ('somthing went wrong'))
 }
-
-fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=2aa5de91e2d58f7baa00946e1b387784")
-.then(response => response.json())
-.then(data =>{
-  //temp //
-  for(i=0; i<5; i++) {
-    document.getElementById("day" + (i+1)+"Temp").innerHTML = "Temp:" + Number(data.list[i].main.temp_min -290.62).toFixed(1)+"°";
-  }
-//wind speed //
-  for(i=0; i<5; i++) {
-    document.getElementById("day" + (i+1)+"Wind").innerHTML = "Wind:" + Number(data.list[i].wind.speed -0.82).toFixed(1)+"%";
-  }
-// humidity //
-  for(i=0; i<5; i++) {
-    document.getElementById("day" + (i+1)+"Humidity").innerHTML = "Humidity" + Number(data.list[i].main.humidity -86).toFixed(1)+"°";
-  }
-
-// image //
-for(i=0; i<5; i++) {
-  document.getElementById("img" + (i+1)).src = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + ".png";
-}
-
-for(i=0; i<5; i++) {
-  document.getElementById('day' + (i+1)).innerHTML = weekday[checkDay(i)];
-}
-
-})
-
-.catch(err =>('somthing went wrong'))
 
 function defaultScreen() {
   document.getElementById("cityInput").defaultvalue = "minneapolis";
   getInfo();
 }
 const d = new Date();
-const weekday = ['Sunday', 'Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',];
+const weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',];
 
 function checkDay(day) {
-if (day + d.getDay() > 6) {
-  return day + d.getDay()-7;
-}
-else{
-  return day + d.getDay();
-}
+  if (day + d.getDay() > 6) {
+    return day + d.getDay() - 7;
+  }
+  else {
+    return day + d.getDay();
+  }
 
 };
-searchButton.on('click', function (event){
+cityName.addEventListener('click', function (event) {
   event.preventDefault();
-  console.log('click', click)
+  console.log('cityName', cityName)
 
-var cityName = document.getElementById("cityName");
-var newName = localStorage.getItem('newName');
+  var cityName = document.getElementById("cityName");
+  var newName = localStorage.getItem('newName');
 
-cityName.textContent = newName;
-console.log('cityName', cityName)
+  cityName.textContent = newName;
+  console.log('cityName', cityName)
 
-    localStorage.setItem("cityInput", cityInput);
-  });
+  localStorage.setItem("cityInput", cityInput);
+});
 
-
-
-  
